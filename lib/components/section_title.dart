fc623b913479a47bc74b291a65eaa40700999d97
tamhoang <ngoctam121191@yahoo.com>
2021-02-21 20:05:29 +0700

    initialize

---
+++ lib/components/section_title.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/constants.dart';
+import 'package:travel_app/size_config.dart';
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    Key key,
+    @required this.title,
+    @required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(kDefaultPadding),
+      ),
+      child: Row(
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: getProportionateScreenWidth(16),
+            ),
+          ),
+          Spacer(),
+          GestureDetector(
+            onTap: press,
+            child: Text("See All"),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/components/popular_places.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/components/place_card.dart';
+import 'package:travel_app/components/section_title.dart';
+import 'package:travel_app/constants.dart';
+import 'package:travel_app/models/TravelSpot.dart';
+import 'package:travel_app/size_config.dart';
+
+class PopularPlaces extends StatelessWidget {
+  const PopularPlaces({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SectionTitle(
+          title: "Right Now At Spark",
+          press: () {},
+        ),
+        VerticalSpacing(of: 20),
+        SingleChildScrollView(
+          clipBehavior: Clip.none,
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              ...List.generate(
+                travelSpots.length,
+                (index) => Padding(
+                  padding: EdgeInsets.only(
+                      left: getProportionateScreenWidth(kDefaultPadding)),
+                  child: PlaceCard(
+                    travelSport: travelSpots[index],
+                    press: () {},
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: getProportionateScreenWidth(kDefaultPadding),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
